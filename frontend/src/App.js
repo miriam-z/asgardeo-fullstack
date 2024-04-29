141d32b5329ca9956ca4f55b83b4e1aa7c702be5
miriam-z <miriam_z@icloud.com>
2024-04-30 03:32:53 +0800

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [weatherData, setWeatherData] = useState(null);
+
+  const REACT_APP_BACKEND_URL = "http://127.0.0.1:8000"
+
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      try {
+        const response = await axios.get(`${REACT_APP_BACKEND_URL}/weather`);
+        setWeatherData(response.data);
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+      }
+    };
+    fetchWeatherData();
+  }, []);
+
+  return (
+    <div>
+      {weatherData ? (
+        <div>
+          <h1>Current Weather in London</h1>
+          <p>Temperature: {weatherData.main.temp}°C</p>
+          <p>Description: {weatherData.weather[0].description}</p>
+        </div>
+      ) : (
+        <p>Loading weather data...</p>
+      )}
+    </div>
+  );
+}
+
+export default App;
